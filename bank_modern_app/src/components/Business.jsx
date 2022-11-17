23b1f89107ec1d269ce0c21ddcf3f601f9d002c7
Aditya <aditya.freetheverse@gmail.com>
2022-11-17 15:52:12 +0530

    third day commit

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -5,7 +5,9 @@ import Button from './Button';
 
 const FeatureCard = ({ icon, title, content, index }) => (
   <div>
-
+      <div>
+        <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+      </div>
   </div>
 
 )
