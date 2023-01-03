c88c56f27a6406ba5b58e7e32bcf2812747527b0
Aditya <aditya.freetheverse@gmail.com>
2023-01-03 17:05:40 +0530

    happy new year

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -4,9 +4,13 @@ import styles, { layout } from '../styles';
 
 
 const Billing = () => (
-    <section id="">
+    <section id="product" className={layout.sectionReverse}>
 
-      
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      </div>
+
+
     </section>
   )
 
