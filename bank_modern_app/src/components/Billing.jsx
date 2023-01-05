4ab28925bb82ae9af905dd203887127f38498042
Aditya <aditya.freetheverse@gmail.com>
2023-01-05 17:10:57 +0530

    yellow

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -10,6 +10,12 @@ const Billing = () => (
         <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
       </div>
 
+      <div className="absolute z-[3] -left-1/3 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      
+      <div className="absolute z-[3] -left-1/3 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+
+      <div />
+
 
     </section>
   )
