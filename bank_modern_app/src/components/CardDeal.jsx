2d062b91386168c52d63f9d080b3e94f195a65bb
Aditya <aditya.freetheverse@gmail.com>
2023-01-09 23:40:31 +0530

    commitY

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+import { card } from '../assets';
+import styles, { layout } from '../styles';
+import Button from './Button';
 
-export default CardDeal
+
+const CardDeal = () => (
+
+    <section className={layout.section}>
+        <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>The banking revolution combining the experience of WEB2 with the ingenuity of WEB3.</p>
+          <Button styles="mt-10" />
+        </div>
+
+       
+
+    </section>
+
+  );
+
+
+export default CardDeal;
