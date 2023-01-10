978a59fdb6fb836dcb17b9d68ccebc39b5f24182
Aditya <aditya.freetheverse@gmail.com>
2023-01-10 14:23:52 +0530

    yes2

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -7,17 +7,21 @@ import Button from './Button';
 const CardDeal = () => (
 
     <section className={layout.section}>
+      
         <div className={layout.sectionInfo}>
           <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
           <p className={`${styles.paragraph} max-w-[470px] mt-5`}>The banking revolution combining the experience of WEB2 with the ingenuity of WEB3.</p>
           <Button styles="mt-10" />
+
         </div>
 
-       
+        <div>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+        </div>  
 
     </section>
 
   );
 
 
-export default CardDeal;
+export default CardDeal
